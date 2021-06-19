29836d6afc3891039fb9c2af0973fde36a131f9d
ISABEL TONY <isabel.tony.e@gmail.com>
2021-06-19 12:45:50 -0300

    creacion de las tablas clientes colore y tipos

---
+++ database/migrations/2021_06_19_153130_create_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 50);
+            $table->string('eu', 10);
+            $table->unsignedBigInteger('colour_id');
+            $table->foreign('colour_id')->references('id')->on('colours')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
