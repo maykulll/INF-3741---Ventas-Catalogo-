527bd6891884fccf33903040947532ec7fe7d30b
ISABEL TONY <isabel.tony.e@gmail.com>
2021-06-22 18:29:28 -0300

    creacion de tablas producto

---
+++ database/migrations/2021_06_19_155234_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('product', 50);
+            $table->double('prize', 8.2);
+            $table->string('imageurl');
+            $table->string('brand', 50);
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
