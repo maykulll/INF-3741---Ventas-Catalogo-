1d3388cc03b5f098749332fed9608e671b28b4c8
ISABEL TONY <isabel.tony.e@gmail.com>
2021-07-06 12:06:21 -0300

    vistas cliente productos

--- routes/cliente.php
+++ routes/cliente.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Cliente\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\Cliente\HomeController::class, 'index'])->name('cliente.home');
+Route::resource('/productos', ProductoController::class)->names('cliente.producto');
